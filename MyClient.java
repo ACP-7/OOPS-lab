3f68482e0837036001826daa14272ca9665f19fe
Akshay Chandra Panicker <129216864+ACP-7@users.noreply.github.com>
2026-04-06 14:48:59 +0530

    Add files via upload

---
+++ MyClient.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+
+public class MyClient {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 7011);
+             DataOutputStream dout = new DataOutputStream(socket.getOutputStream())) {
+
+            dout.writeUTF("Client Call!!!");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
